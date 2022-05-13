c3bc30e64e334e8be03d1e942d7e9e8bb8253e82
33168相沢至 <33168@ojs.ac.jp>
2022-05-13 15:17:03 +0900

    ラムダ演算子

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -44,5 +44,7 @@ namespace Exercise03
                 }
             }
         }
+
+        
     }
 }
---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01
+{
+    class Program
+    {
+        public delegate bool judgement(int value);//デリゲートの宣言
+
+        static void Main(string[] args)
+        {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            var names = list.FindAll(s => s.Length <= 6);
+
+            names.ForEach(s => Console.WriteLine(s));
+
+            /*   ↑でも可
+             *  foreach (var s in names)
+             *  {
+             *      Console.WriteLine(names);
+             *  }
+            */
+        }
+    }
+}
