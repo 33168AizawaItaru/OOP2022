79396bfa24e0f0d24dec511fbd8b5689b9a14df5
33168相沢至 <33168@ojs.ac.jp>
2022-05-20 14:24:47 +0900

    問題4.1.1完成

---
+++ Chapter04/Exercise01/YearMonth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    class YearMonth
+    {
+        //年プロパティ
+        public int Year { get; private set; }
+
+        //月プロパティ
+        public int Month { get; private set; }
+
+        //コンストラクタ
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+    }
+}
