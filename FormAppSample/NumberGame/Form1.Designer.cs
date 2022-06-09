8799616e88be53b666c769cf1a3363a9934a1b38
33168相沢至 <33168@ojs.ac.jp>
2022-06-09 14:15:18 +0900

    完成

---
+++ FormAppSample/NumberGame/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace NumberGame
+{
+    partial class form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inRandom = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.inRandom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ 明朝", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(320, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(617, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "の中から数字を当ててね(*。*)=b";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("ＭＳ 明朝", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(246, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 107);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "入力";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ 明朝", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(683, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "がんば";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // inRandom
+            // 
+            this.inRandom.Font = new System.Drawing.Font("ＭＳ 明朝", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.inRandom.Location = new System.Drawing.Point(155, 252);
+            this.inRandom.Name = "inRandom";
+            this.inRandom.Size = new System.Drawing.Size(329, 74);
+            this.inRandom.TabIndex = 3;
+            this.inRandom.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("ＭＳ 明朝", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(73, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 40);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "1から";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("ＭＳ 明朝", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(223, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 40);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "50";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.ClientSize = new System.Drawing.Size(961, 570);
+            this.Controls.Add(this.inRandom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.inRandom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown inRandom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FormAppSample/NumberGame/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGame
+{
+    public partial class form1 : Form
+    {
+        private Random rand = new Random();
+        private int numRand;
+
+        public form1(){InitializeComponent();}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (numRand == inRandom.Value)
+            {
+                label2.Text = "正解";
+            } else if (inRandom.Value > numRand)
+            {
+                label2.Text = "小さくして";
+            } else
+            {
+                label2.Text = "大きくして";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numRand = rand.Next(minValue: 1, maxValue: 51);
+        }
+
+        public void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            getRandom();
+        }
+
+        private void getRandom() {
+            numRand = rand.Next(minValue: 1, maxValue: 51);
+        }
+
+        private void label1_Click(object sender, EventArgs e){}
+        private void label2_Click(object sender, EventArgs e){}
+    }
+}
