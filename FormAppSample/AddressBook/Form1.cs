cff4e0a5b1d061995ba04e12b036ada21d6dee87
33168相沢至 <33168@ojs.ac.jp>
2022-06-09 15:01:01 +0900

    デザイン完成

--- FormAppSample/AddressBook/Form1.cs
+++ FormAppSample/AddressBook/Form1.cs
@@ -16,5 +16,11 @@ namespace AddressBook
         {
             InitializeComponent();
         }
+        
+
+
+
+        private void label1_Click(object sender, EventArgs e){}
+        private void textBox3_TextChanged(object sender, EventArgs e){}
     }
 }
