8a07975289985a1418213b0f08bb2c6ad8a1ecbf
33168相沢至 <33168@ojs.ac.jp>
2022-06-14 15:55:04 +0900

    完成

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -22,21 +22,29 @@ namespace Test01 {
             foreach (var line in lines)
             {
                 string[] items = line.Split(',');
-                Student student
+                Student student = new Student
+                {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(student);
             }
-
-
-
+            return scores;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
 
-
-
-
-
-            
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score)
+            {
+                if (dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
+            }
+            return dict;
         }
     }
 }
