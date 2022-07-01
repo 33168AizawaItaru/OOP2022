d24cc6ae54ab8323063b5f3ee69842c6645e700c
33168相沢至 <33168@ojs.ac.jp>
2022-07-01 13:25:02 +0900

    Rssreader始め

---
+++ Chapter11/XmlSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace XmlSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+            var stream = new WebClient().OpenRead("https://news.yahoo.co.jp/rss/media/abn/all.xml");
+
+            var xdoc = XDocument.Load(stream);
+            var xNews = xdoc.Root.Descendants("item");
+            foreach (var data in xNews)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
