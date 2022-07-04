c8ca7fdffd76b48c4c87302e7ba7ada8c97663d7
33168相沢至 <33168@ojs.ac.jp>
2022-07-04 11:58:24 +0900

    0704終了

---
+++ Chapter11/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+        }
+
+        private static void Exercise1_1(string file)
+        {
+            var xdoc = XDocument.Load(file);
+
+            var samples = xdoc.Root.Elements().Select(x=> new { 
+                                                        Name = (string)x.Element("name"),
+                                                        TeamMenber = (string)x.Element("teammembers")});
+            foreach (var sample in samples)
+            {
+                Console.WriteLine("{0} {1}",sample.Name,sample.TeamMenber);
+            }
+        }
+
+        private static void Exercise1_2(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var samples = xdoc.Root.Elements().Select(x => new {
+                                                        Name = (string)x.Element("name").Attribute("kanji"),
+                                                        Firstplayed = (string)x.Element("firstplayed")}
+                                                     ).OrderBy(x=>x.Firstplayed);
+
+            foreach (var sample in samples)
+            {
+                Console.WriteLine("{0}({1})", sample.Name, sample.Firstplayed);
+            }
+        }
+
+        private static void Exercise1_3(string file)
+        {
+            var xdoc = XDocument.Load(file);
+
+            
+        }
+
+        private static void Exercise1_4(string file, string newfile)
+        {
+            
+        }
+    }
+}
--- Chapter11/Exercise1/Exercise1/Properties/AssemblyInfo.cs
+++ Chapter11/Exercise1/Exercise1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("XmlSample")]
+[assembly: AssemblyTitle("Exercise1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XmlSample")]
+[assembly: AssemblyProduct("Exercise1")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("074507d6-0601-4336-8583-f29e52c3de84")]
+[assembly: Guid("96519d05-ccdb-4a59-a681-e6c4f0f9cd18")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
