2a8117cb633d53998b520a04c055ea2ab0a6e5c9
33168相沢至 <33168@ojs.ac.jp>
2022-08-29 11:57:32 +0900

    0829終了

--- Chapter17/Exercise1/Exercise1/Program.cs
+++ Chapter17/Exercise1/Exercise1/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextFileProcessor;
 
-namespace TextFileProcessor
+namespace Exercise1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TextProcessor.Run<ToHankakuProcessor>(@"\\172.16.45.1\学生共有\#infosys2022\オブジェクト指向プログラミング\２期目\Sample.txt");
         }
     }
 }
