1c5e00768b02f951a8be56fa1f928563e5ac7814
33168相沢至 <33168@ojs.ac.jp>
2022-08-30 15:57:54 +0900

    0830終了

---
+++ Chapter17/Strategy/DistanceConverter/ConverterBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter
+{
+    public abstract class ConverterBase
+    {
+        public abstract bool IsMyUnit(string name);
+
+        //メートルとの比率
+        protected abstract double Ratio { get; }
+
+        //距離の単位名
+        public abstract string UnitName { get; }
+
+        //メートルからの変換
+        public double FromMater(double mater)
+        {
+            return mater / Ratio;
+        }
+
+        //メートルへの変換
+        public double ToMater(double feet)
+        {
+            return feet * Ratio;
+        }
+
+    }
+}
