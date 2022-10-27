9e0eac6b68579a72494b2c57a562aedc0cca2fe2
33168相沢至 <33168@ojs.ac.jp>
2022-10-27 11:49:33 +0900

    CollarChecker完成

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,7 @@ namespace CollarChecker
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = GetColorList();
         }
 
         private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -42,10 +44,51 @@ namespace CollarChecker
 
         private void getColor()
         {
-            var rCol = Slider1.Value;
-            var gCol = Slider2.Value;
-            var bCol = Slider3.Value;
-            label.Background = new SolidColorBrush(Color.FromRgb((byte)rCol, (byte)gCol, (byte)bCol));
+            var r = Slider1.Value;
+            var g = Slider2.Value;
+            var b = Slider3.Value;
+            label.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+        }
+
+        /// <summary>
+        /// すべての色を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList()
+        {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        /// <summary>
+        /// 色と色名を保持するクラス
+        /// </summary>
+        public class MyColor
+        {
+            public Color Color { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void Border_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            var name = mycolor.Name;
+
+            label.Background = new SolidColorBrush(color);
+
+            var R = color.R;
+            var G = color.G;
+            var B = color.B;
+
+            Slider1.Value = R;
+            Slider2.Value = G;
+            Slider3.Value = B;
         }
     }
 }
