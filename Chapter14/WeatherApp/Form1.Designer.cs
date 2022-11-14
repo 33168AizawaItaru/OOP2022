335fdc3eac190da57c758ecd861a9a05654b32a7
33168相沢至 <33168@ojs.ac.jp>
2022-11-14 15:56:24 +0900

    11/14終了 未完成

--- Chapter14/WeatherApp/Form1.Designer.cs
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -29,36 +29,26 @@ namespace WeatherApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tbWeatherInfo = new System.Windows.Forms.TextBox();
-            this.btWeatherGet = new System.Windows.Forms.Button();
             this.Prefecture = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // tbWeatherInfo
             // 
-            this.tbWeatherInfo.Location = new System.Drawing.Point(136, 219);
+            this.tbWeatherInfo.Font = new System.Drawing.Font("ＭＳ 明朝", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbWeatherInfo.Location = new System.Drawing.Point(344, 522);
             this.tbWeatherInfo.Multiline = true;
             this.tbWeatherInfo.Name = "tbWeatherInfo";
             this.tbWeatherInfo.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.tbWeatherInfo.Size = new System.Drawing.Size(558, 218);
+            this.tbWeatherInfo.Size = new System.Drawing.Size(582, 129);
             this.tbWeatherInfo.TabIndex = 0;
-            // 
-            // btWeatherGet
-            // 
-            this.btWeatherGet.Font = new System.Drawing.Font("ＭＳ 明朝", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btWeatherGet.Location = new System.Drawing.Point(12, 395);
-            this.btWeatherGet.Name = "btWeatherGet";
-            this.btWeatherGet.Size = new System.Drawing.Size(118, 42);
-            this.btWeatherGet.TabIndex = 1;
-            this.btWeatherGet.Text = "取得";
-            this.btWeatherGet.UseVisualStyleBackColor = true;
-            this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
             // 
             // Prefecture
             // 
             this.Prefecture.Font = new System.Drawing.Font("ＭＳ 明朝", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Prefecture.FormattingEnabled = true;
-            this.Prefecture.Location = new System.Drawing.Point(136, 54);
+            this.Prefecture.Location = new System.Drawing.Point(12, 12);
             this.Prefecture.Name = "Prefecture";
             this.Prefecture.Size = new System.Drawing.Size(235, 24);
             this.Prefecture.TabIndex = 2;
@@ -68,12 +58,16 @@ namespace WeatherApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 440);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(938, 663);
             this.Controls.Add(this.Prefecture);
-            this.Controls.Add(this.btWeatherGet);
             this.Controls.Add(this.tbWeatherInfo);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,7 +76,6 @@ namespace WeatherApp
         #endregion
 
         private System.Windows.Forms.TextBox tbWeatherInfo;
-        private System.Windows.Forms.Button btWeatherGet;
         private System.Windows.Forms.ComboBox Prefecture;
     }
 }
--- Chapter14/WeatherApp/Form1.cs
+++ Chapter14/WeatherApp/Form1.cs
@@ -14,6 +14,7 @@ namespace WeatherApp
 {
     public partial class Form1 : Form
     {
+        Image currentImage = Image.FromFile(@"C:\Users\infosys\Downloads\touka.jpg");
         public Form1()
         {
             InitializeComponent();
@@ -39,21 +40,379 @@ namespace WeatherApp
             Prefecture.Items.AddRange(addPrefecture);
         }
 
-        private void btWeatherGet_Click(object sender, EventArgs e)
+        private void Prefecture_SelectedIndexChanged(object sender, EventArgs e)
         {
             var wc = new WebClient()
             {
                 Encoding = Encoding.UTF8
             };
 
-            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/200000.json");
+            string dString;
+            Rootobject json;
 
-            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+            switch (Prefecture.SelectedItem)
+            {
+                case "宗谷地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/011000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
 
-            tbWeatherInfo.Text = json.text;
+                case "上川・留萌地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/012000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "網走・北見・紋別地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/013000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "十勝地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/014030.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "釧路・根室地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/014100.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "胆振・日高地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/015000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "石狩・空知・後志地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/016000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "渡島・檜山地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/017000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "青森県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/020000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "岩手県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/030000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "宮城県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/040000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "秋田県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/050000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "山形県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/060000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "福島県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/070000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "茨城県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/080000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "栃木県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/090000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "群馬県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/100000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "埼玉県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/110000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "千葉県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/120000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "東京都":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "神奈川県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/140000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "山梨県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/190000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "長野県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/200000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "岐阜県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/210000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "静岡県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/220000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "愛知県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/230000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "三重県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/240000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "新潟県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/150000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "富山県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/160000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "石川県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/170000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "福井県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/180000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "滋賀県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/250000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "京都府":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/260000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "大阪府":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/270000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "兵庫県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/280000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "奈良県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/290000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "和歌山県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/300000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "鳥取県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/310000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "島根県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/320000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "岡山県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/330000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "広島県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/340000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "徳島県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/360000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "香川県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/370000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "愛媛県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/380000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "高知県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/390000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "山口県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/350000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "福岡県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/400000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "佐賀県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/410000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "長崎県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/420000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "熊本県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/430000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "大分県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/440000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "宮崎県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/450000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "奄美地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/460040.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "鹿児島県":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/460100.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "沖縄本島地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/471000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "大東島地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/472000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "宮古島地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/473000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+
+                case "八重山地方":
+                dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/474000.json");
+                json = JsonConvert.DeserializeObject<Rootobject>(dString);
+                tbWeatherInfo.Text = json.text;
+                break;
+            }
         }
 
-        private void Prefecture_SelectedIndexChanged(object sender, EventArgs e)
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            if (currentImage != null)
+            {
+                //　画像を0,0に描画する
+                e.Graphics.DrawImage(currentImage, 0, 0, currentImage.Width, currentImage.Height);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
         {
             
         }
