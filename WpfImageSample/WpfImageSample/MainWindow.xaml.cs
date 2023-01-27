f342e8a894f5e42f843d8e29b55806490e9dfbc4
33168相沢至 <33168@ojs.ac.jp>
2023-01-27 14:51:55 +0900

    0127master変更のため

---
+++ WpfImageSample/WpfImageSample/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfImageSample {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        
+        BitmapImage m_bitmap = null;
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var path = "https://static.tenki.jp/images/icon/forecast-days-weather/03.png";
+
+            m_bitmap = new BitmapImage();
+            m_bitmap.BeginInit();
+            m_bitmap.UriSource = new Uri(path);
+            m_bitmap.EndInit();
+            imageDisp.Source = m_bitmap;
+
+        }
+    }
+}
